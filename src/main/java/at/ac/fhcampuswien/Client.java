1483d4e929c0e5f3b767ff3a513e74e6072e7738
Eler W <eler.wohlmuth@stud.fh-campuswien.ac.at>
2020-12-18 18:00:20 +0100

    Implemented sockets

--- src/main/java/at/ac/fhcampuswien/Client.java
+++ src/main/java/at/ac/fhcampuswien/Client.java
@@ -1,4 +1,34 @@
 package at.ac.fhcampuswien;
 
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
 public class Client {
+    public static void main(String[] args) {
+
+
+        try {
+            Socket socket = new Socket("localhost", 6666);
+            System.out.println("Connected.");
+
+
+            DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
+
+            DataInputStream inputStream = new DataInputStream(socket.getInputStream());
+            String message = "Hello Gefaehrten";
+            outputStream.writeUTF(message);
+            outputStream.flush();
+
+            String string = (String) inputStream.readUTF();
+            System.out.println(string);
+
+        } catch (IOException e) {
+            System.out.println("Error 1");
+        }
+
+
+    }
 }
--- src/main/java/at/ac/fhcampuswien/Server.java
+++ src/main/java/at/ac/fhcampuswien/Server.java
@@ -1,4 +1,34 @@
 package at.ac.fhcampuswien;
 
+import javax.sound.midi.SysexMessage;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(6666); //defines Port of ServerSocket
+            System.out.println("Ready for connection");
+            Socket socket = serverSocket.accept(); //listens to ServerSocket Port (6666) and connects
+
+            System.out.println("Client connected");
+            DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream()); //forwards input on Client
+
+            DataInputStream inputStream = new DataInputStream(socket.getInputStream());
+            String message = (String) inputStream.readUTF();
+
+            String string = message;
+            outputStream.writeUTF(string);
+            outputStream.flush();
+            outputStream.close();
+
+
+        } catch (IOException e) {
+            System.out.println("Error 2");
+        }
+
+    }
 }
